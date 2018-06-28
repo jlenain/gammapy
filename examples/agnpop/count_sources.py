f9e17c66e361bbb5f63376a37e2d5381da5cfc74
jlenain <jeanphilippe.lenain@free.fr>
2018-06-28 15:12:40 +0200

    Implemented skyplots of simulated and detected sources

--- examples/agnpop/count_sources.py
+++ examples/agnpop/count_sources.py
@@ -3,6 +3,8 @@ from astropy.io import ascii
 import numpy as np
 from itertools import product
 from matplotlib import pyplot as plt
+from astropy.coordinates import SkyCoord
+from astropy import units as u
 
 d = ascii.read('results.txt')
 
@@ -31,6 +33,29 @@ for m,s,l,e in product(models, sites, livetime, emin):
         ax.hist(d2['Redshift'], color='r', label='Detected sources')
         plt.legend()
         filename = './fig/histz_{}_{}_Eth{}_{}.png'.format(s, l, e, m)
+        plt.savefig(filename)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='aitoff')
+        c1 = SkyCoord(ra=d1['RAJ2000']*u.degree, dec=d1['DEJ2000']*u.degree, frame='icrs')
+        l1 = c1.galactic.l.wrap_at(180*u.degree)
+        b1 = c1.galactic.b.wrap_at(180*u.degree)
+        ax.plot(-l1.radian,  # East to the left
+                b1.radian,
+                'o',
+                color='b',
+                label='All simulated sources')
+        c2 = SkyCoord(ra=d2['RAJ2000']*u.degree, dec=d2['DEJ2000']*u.degree, frame='icrs')
+        l2 = c2.galactic.l.wrap_at(180*u.degree)
+        b2 = c2.galactic.b.wrap_at(180*u.degree)
+        ax.plot(-l2.radian,  # East to the left
+                b2.radian,
+                'x',
+                color='r',
+                label='Detected sources')
+        plt.legend(loc='upper right', bbox_to_anchor=(1., 1.3))
+        ax.grid(True)
+        filename = './fig/skyplot_{}_{}_Eth{}_{}.png'.format(s, l, e, m)
         plt.savefig(filename)
 
 
